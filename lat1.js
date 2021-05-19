26b3cfd4af70cb74bdcd5306117b459f601b468a
lordgent <lorjenx08@gmail.com>
2021-05-19 19:54:38 +0700

    array js looping jumlah hari dan menampilkan data berdasarkan hari

---
+++ lat1.js
@@ -0,0 +1,13 @@
+const hari = ["senin", "selasa", "Rabu", "Kamis",
+                 "Jumat", "Sabtu","Minggu"];
+
+const jmlh = 6;
+
+
+for(let i = 0; i <= jmlh; i++) {
+
+console.log("hari ke " + i +  ' adalah ' + hari[i] )
+console.log('\n');
+
+}
+console.log("total Jumlah hari : " + hari.length);
