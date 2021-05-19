c2b3671f67264ecff1f5611607312aec31398a48
lordgent <lorjenx08@gmail.com>
2021-05-19 20:45:39 +0700

    update data

--- lat2.js
+++ lat2.js
@@ -2,9 +2,30 @@
 const hari = ["senin", "selasa", "Rabu", "Kamis",
                  "Jumat", "Sabtu","Minggu"];
 const minggu  = 4;
+const bulan = ["Januari", "Februari", "Maret" + "April", "Mei",
+                "Juni", "Juli", "Agustus", "September", "Oktober",
+                "November", "Desember"];
 
 for(let i = 0; i < hari.length; i++) {
     console.log("minggu ke 1 " + "Bus ke " + i + " Berjalan dengan baik di Hari " + hari[i]  )
 }
 console.log("Total bus : " + hari.length);
 console.log("======================")
+console.log('\n')
+
+//data mingguan operasi hanya bus 2 yang berjalan
+
+for(let j = 1; j <= minggu; j++) {
+    if(j === 2){
+        console.log("bus " + j + " pada minggu ke " + j + " Berjalan dengan baik")
+    } else {
+        console.log("bus " + j +" pada minggu ke " + j + " tidak beroperasi")
+    }
+}
+
+//data bulanan
+
+let k = 0;
+while(k <= bulan.length) {
+
+}
