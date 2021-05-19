dc9469814a96cb680d9427c0ad70e2a747600ce1
lordgent <lorjenx08@gmail.com>
2021-05-19 20:08:11 +0700

    menampilkan kondisi dan hari pada data saat beroperasi

---
+++ lat2.js
@@ -0,0 +1,10 @@
+//program menampilkan jumlah bus dan bus beroperasi
+const hari = ["senin", "selasa", "Rabu", "Kamis",
+                 "Jumat", "Sabtu","Minggu"];
+const minggu  = 4;
+
+for(let i = 0; i < hari.length; i++) {
+    console.log("minggu ke 1 " + "Bus ke " + i + " Berjalan dengan baik di Hari " + hari[i]  )
+}
+console.log("Total bus : " + hari.length);
+console.log("======================")
